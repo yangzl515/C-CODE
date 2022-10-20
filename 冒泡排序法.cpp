bb7bf088a4d86e400b493370b70ea2f72766ac05
ZhongLin_Y <105596007+yangzl515@users.noreply.github.com>
2022-10-20 08:11:56 +0800

    Add files via upload

---
+++ 冒泡排序法.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#define N 10
+int main()
+{
+	int a[10], i, j, t;
+	printf("please input 10 numbers:\n");
+	for (i = 0; i <10; i++)
+		scanf_s("%d", &a[i]);
+	
+	for (i = 0; i < N - 1; i++)
+		for(j = 0; j < N - (i + 1);j++)
+			if (a[j] > a[j + 1])
+			{
+				t = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = t;
+			}
+	printf("number in rise order :\n");
+	for (i = 0; i < N; i++)
+		printf("%4d", a[i]);
+}
